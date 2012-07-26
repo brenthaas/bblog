5d13ab246a80d90d33c3592106bb8389b0b0222d
Brent <brenthaas+git@gmail.com>
2012-07-26 15:19:38 -0700

    added login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Bblog::Application.routes.draw do
   # Static Pages
   match "home", to: "static_pages#home"
   match "about", to: "static_pages#about"
-
-
   root to: "static_pages#home"
+
+  # Authentication actions
+  match 'sessions', to: 'sessions#create', via: :post
+  match 'login', to: 'sessions#new', via: :get
+  match 'logout', to: 'sessions#destroy', via: :delete
+
+  # User
+  match "users/:id", to: "users#show", as: :user
 end
