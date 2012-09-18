4e77d70a4590d97961625c5e7ea8a83d491e40fa
Brent <brenthaas+git@gmail.com>
2012-09-18 14:24:30 -0700

    add authorized method to stub out authorizations

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -5,6 +5,13 @@ module LoginHelper
 		fill_in "Password", with: pass
 		click_button "login"
 	end
+
+	def authorized(worked=true)
+		# trying to get stubbing working - no stubbing module/mix-in methods?
+		@helpme = Object.new.extend SessionsHelper
+		@helpme.stub!(:logged_in?).and_return(worked)
+		@helpme.stub(:authorize_or_redirect!).and_return(worked)
+	end 
 end
 
 RSpec.configure do |config|
