b32bd35c53d2796c6c7dfadace2aa1d23db0dae3
Brent <brenthaas+git@gmail.com>
2012-10-08 23:23:07 -0700

    got basic blog functionality working

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,6 +24,12 @@ class BlogsController < ApplicationController
   # POST /blogs
   def create
     @blog = Blog.new(params[:blog])
+    @blog.user_id = @current_user.id
+    if @blog.save
+      redirect_to @blog, message: "Blog successfully saved!"
+    else
+      render action: 'new', message: "Problem saving blog..."
+    end
   end
 
   # PUT /blogs/1
@@ -34,6 +40,10 @@ class BlogsController < ApplicationController
   # DELETE /blogs/1
   def destroy
     @blog = Blog.find(params[:id])
-    @blog.destroy
+    if @blog.destroy
+      redirect_to blogs_path, message: "Successfully destroyed \"#{@blog.title[0,10]}...\""
+    else
+      redirect_to blogs_path, error: "Could not destroy BlogID #{params[:id]}"
+    end
   end
 end
--- spec/views/blogs/index.html.erb_spec.rb
+++ spec/views/blogs/index.html.erb_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe "blogs/index" do
   before(:each) do
-    assign(:blogs, [
-      stub_model(Blog),
-      stub_model(Blog)
-    ])
+    @blogs = [ FactoryGirl.create(:blog, title: "TestBlog 1"),
+    					FactoryGirl.create(:blog, title: "TestBlog 2") ]
   end
 
   it "renders a list of blogs" do
-    render
+    render 
+
+    rendered.should have_selector('h3', class: "post_title", count: @blogs.count)
   end
 end
